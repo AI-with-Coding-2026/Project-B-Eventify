c8de95905af528d5644550351845d832e3d3bf84
MBD <divinemountso@gmail.com>
2026-08-07 21:07:46 +0300

    fix merge conglicts with main

--- Eventify/settings.py
+++ Eventify/settings.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'Eventify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
