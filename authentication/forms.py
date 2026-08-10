2f4f392279e745256b5162c47035cd4fc4fcd858
Mapula Whitney Kataka <katakatee05@gmail.com>
2026-08-10 06:56:18 +0200

    Add first and last name to user registration

--- authentication/forms.py
+++ authentication/forms.py
@@ -5,7 +5,10 @@ from .models import User, UserRole
 
 
 class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     email = forms.EmailField(required=True)
+
     role = forms.ChoiceField(
         choices=[
             (UserRole.ORGANIZER, UserRole.ORGANIZER.label),
@@ -17,18 +20,35 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'role', 'password1', 'password2')
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "role",
+            "password1",
+            "password2",
+        )
 
     def clean_role(self):
-        role = self.cleaned_data['role']
+        role = self.cleaned_data["role"]
+
         if role == UserRole.ADMIN:
-            raise forms.ValidationError('Admin accounts cannot be created through registration.')
+            raise forms.ValidationError(
+                "Admin accounts cannot be created through registration."
+            )
+
         return role
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.role = self.cleaned_data['role']
+
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email"]
+        user.role = self.cleaned_data["role"]
+
         if commit:
             user.save()
+
         return user
