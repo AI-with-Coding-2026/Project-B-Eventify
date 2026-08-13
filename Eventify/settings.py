e17361c945fe96e189beb26399437481211f63cb
Ammar <230044653+Eco6x@users.noreply.github.com>
2026-08-13 23:30:42 +0300

    Update dashboard consistency, card hover animations, and logo integration

--- Eventify/settings.py
+++ Eventify/settings.py
@@ -111,6 +111,9 @@ USE_TZ = True
 # https://djangoproject.com
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Activate the custom user model containing the 3 required roles
 AUTH_USER_MODEL = 'authentication.User'
