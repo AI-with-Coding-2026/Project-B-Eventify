d7eb7f5030d23d10d8fdd7523573ca47fde73747
Lesego6 <lesego6@example.com>
2026-08-15 02:08:31 +0000

    Fix ticket capacity and booking navigation

--- Eventify/settings.py
+++ Eventify/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'authentication.context_processors.dashboard_link',  # تم تعليقها المؤقت
+                'authentication.context_processors.dashboard_link',
             ],
         },
     },
