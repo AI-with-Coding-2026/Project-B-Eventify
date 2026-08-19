7c13be2bc1924b7f88210e823c8afc75ce83a5ab
Amr100 <aaamr5952@gamil.com>
2026-08-19 23:51:35 +0300

    maintain

--- Eventify/settings.py
+++ Eventify/settings.py
@@ -180,7 +180,8 @@ EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=True, cast=bool)
 DEFAULT_FROM_EMAIL = config(
     'DEFAULT_FROM_EMAIL',
     default=EMAIL_HOST_USER or 'Eventify <noreply@eventify.com>',
@@ -199,3 +200,6 @@ CSRF_TRUSTED_ORIGINS = [origin for origin in [SITE_URL] if origin.startswith('ht
 render_host = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if render_host:
     CSRF_TRUSTED_ORIGINS.append(f'https://{render_host}')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
